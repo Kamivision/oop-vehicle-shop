e414557e4f2f97c3c19b30ec4c5fa6cf75eb7469
Kamivision <kamivision@gmail.com>
2026-02-13 17:10:31 -0600

    created menu text, started logic tree, and added a test info file

--- car_management.py
+++ car_management.py
@@ -1,5 +1,20 @@
 from cars import Cars
 
 # Terminal Application that interacts with the car class
-def menu():
-       
+while True:
+    mode = input("""
+----  Welcome  ----
+1. Add a car
+2. View all cars
+3. View total number of cars
+4. See a car's details
+5. Service a car
+6. Update mileage
+7. Quit
+-------------------
+Please select a number:
+    """)
+    if mode == '1':
+        pass
+    elif mode == '7':
+        break   
--- cars.py
+++ cars.py
@@ -69,26 +69,4 @@ class Cars:
         if isinstance(services_val, str):
             self._services = services_val
 
-
-list_of_cars = [
-   {'id_num': 1, 'make': 'Nissan', 'model': 'Pathfinder', 'year': 2024, 'mileage': 54321, 'services': 'none'},
-   {'id_num': 2, 'make': 'Toyota', 'model': 'Camry', 'year': 2025, 'mileage': 4321, 'services': 'none'},
-   {'id_num': 3, 'make': 'Dodge', 'model': 'Neon', 'year': 1999, 'mileage': 654321, 'services': 'New Engine'}
-]
     
-car_instances = []
-
-for car in list_of_cars:
-    car_instances.append(
-        Cars(
-            id_num = car.get('id_num'), 
-            make = car.get('make'), 
-            model = car.get('model') , 
-            year = car.get('year'), 
-            mileage = car.get('mileage'), 
-            services = car.get('services')
-         )
-    )
-    
-
-print(car_instances)   
